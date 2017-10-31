f209bd92734e8f3846912944abd50467b3920250
Welle Charlotte <charlottewelle@yahoo.fr>
2017-10-31 07:34:57 +0100

    Add unit/integration test

--- src/test/java/aka/jwowjpa/test/Item_Test.java
+++ src/test/java/aka/jwowjpa/test/Item_Test.java
@@ -3,17 +3,25 @@ package aka.jwowjpa.test;
 import java.util.List;
 import java.util.logging.Logger;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 
 import aka.jwowjpa.persistence.controllers.ItemController;
 import aka.jwowjpa.persistence.models.Item;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:application-test-context.xml")
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
 public class Item_Test {
 
     private final Logger logger = Logger.getLogger("myLog");
@@ -22,20 +30,19 @@ public class Item_Test {
 
     @Before
     public void setUp() {
-        final ApplicationContext ctx = new ClassPathXmlApplicationContext(
-                "application-test-context.xml");
+        final ApplicationContext ctx = new ClassPathXmlApplicationContext("application-test-context.xml");
 
         this.itemController = ctx.getBean(ItemController.class);
     }
 
     @Test
+    @DatabaseSetup("/dataset/itemRepository.xml")
     public void happyPathScenario() {
-        final Item item = new Item();
-        item.setName("MY ITEM NAME");
+        final List<Item> cars = this.itemController.getItems();
     }
 
-    @Test
-    public void listCarsTest() {
+//    @Test
+//    public void listCarsTest() {
 //
 //
 //        final UserManager userManager = (UserManager) ctx.getBean("userManagerImpl");
@@ -51,23 +58,23 @@ public class Item_Test {
 //
 //        list = userManager.findAllUsers();
 //        System.out.println("User count: " + list.size());
-        final List<Item> cars = this.itemController.getItems();
-//          logger.info("Cars: " + cars.size());
-        Assert.assertNotNull(cars);
-
-        final List<Item> test = this.itemController.getItemByNameLike("Bo");
-        for (final Item item : test) {
-            System.err.println("[Item_Test] listCarsTest - " + item.getName());
-        }
-        final List<Item> test2 = this.itemController.getItemById(Long.valueOf(-1));
-        for (final Item item : test2) {
-            System.err.println("[Item_Test] listCarsTest - " + item.getName());
-        }
-
-    }
-
-    @Test
-    public void getCarTest() {
-//        final List<Item> car = this.carDao.getItemById(this.id);
-    }
+//        final List<Item> cars = this.itemController.getItems();
+////          logger.info("Cars: " + cars.size());
+//        Assert.assertNotNull(cars);
+//
+//        final List<Item> test = this.itemController.getItemByNameLike("Bo");
+//        for (final Item item : test) {
+//            System.err.println("[Item_Test] listCarsTest - " + item.getName());
+//        }
+//        final List<Item> test2 = this.itemController.getItemById(Long.valueOf(-1));
+//        for (final Item item : test2) {
+//            System.err.println("[Item_Test] listCarsTest - " + item.getName());
+//        }
+//
+//    }
+//
+//    @Test
+//    public void getCarTest() {
+////        final List<Item> car = this.carDao.getItemById(this.id);
+//    }
 }
