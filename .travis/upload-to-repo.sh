d06e264bcada9cbcb7a6c21eb97040c70252b616
Welle Charlotte <charlottewelle@yahoo.fr>
2017-11-03 08:48:01 +0100

    Changes

--- .travis/upload-to-repo.sh
+++ .travis/upload-to-repo.sh
@@ -5,7 +5,6 @@ git config --global user.name "Welle Charlotte"
 cd /home/travis/repository
 git init
 git add -f .
-git remote rm origin
 git remote add origin https://welle:$GITHUB_API_KEY@github.com/welle/maven-repostitory.git
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
